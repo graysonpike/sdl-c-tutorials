b29927cbcbae3067bffcf7b7a7344dc1624ce3a1
Grayson112233 <graysonpike@Graysons-MBP.attlocal.net>
2016-05-03 20:19:26 -0500

    Added red rectangle to surface

--- main.c
+++ main.c
@@ -6,11 +6,14 @@
 #define WIDTH 640
 #define HEIGHT 480
 
-int main() {
+int main(int argc, char *argv[]) {
 
-	SDL_Init(SDL_INIT_EVERYTHING);
+	if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("Error: Failed to init SDL2: %s\n", SDL_GetError());
+    }
 
-	SDL_Window *window; 
+	SDL_Window *window;
+    SDL_Surface *surface;
 
 	window = SDL_CreateWindow(
         "SDL2 Test",                  // window title
@@ -22,10 +25,15 @@ int main() {
     );
 
     if (window == NULL) {
-        // In the case that the window could not be made...
         printf("Could not create window: %s\n", SDL_GetError());
         return 1;
     }
+
+    surface = SDL_GetWindowSurface(window);
+
+    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0, 0, 255));
+
+    SDL_UpdateWindowSurface(window);
 
     SDL_Delay(3000);
 
